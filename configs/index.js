5057ad208688e65ef219e6fb92376782d4534c6f
vskokan <victoriaskokan@gmail.com>
2021-01-25 19:53:31 +0300

    Add function for adding full review with transaction

--- configs/index.js
+++ configs/index.js
@@ -1,4 +1,5 @@
-var multer  = require('multer')
+const multer  = require('multer')
+const fs = require('fs')
     
     const storageConfig = multer.diskStorage({
         destination: (req, file, cb) =>{
@@ -6,8 +7,17 @@ var multer  = require('multer')
                 cb(null, "uploads/fishes/")
             if (file.fieldname == "avatar")
                 cb(null, "uploads/users/")
-            if (file.fieldname == "images")
-                cb(null, `uploads/reviews/`)
+            if (file.fieldname == "images") {
+                const user = req.body.login
+
+ 
+                    if (!fs.existsSync(`uploads/reviews/${user}`)) {
+                        return fs.mkdirSync(`uploads/reviews/${user}`)
+                    }
+               
+                cb(null, `uploads/reviews/${user}`)
+            }
+
         },
         filename: (req, file, cb) =>{
             cb(null, Date.now() + file.originalname);
--- controllers/fullReview2-controller.js
+++ controllers/fullReview2-controller.js
@@ -1,5 +1,6 @@
 const client = require('../db')
 const fs = require('fs')
+const format = require('pg-format');
 
 exports.create = (req, res) => {
     const review = {
@@ -13,23 +14,63 @@ exports.create = (req, res) => {
         longitude: req.body.longitude,
         facts: transformFacts(JSON.parse(req.body.facts)),
        // facts: req.body.facts,
-        photos: req.body.files
+        photos: req.files
     }
 
     console.log(review)
+
+    client.query('BEGIN')
+    .then((result) => {
+        return client.query('INSERT INTO reviews (login, date, description, isbaiting, roadquality, fishingtime, raiting, latitude, longitude) VALUES ($1, current_date, $2, $3, $4, $5, $6, $7, $8) returning id',
+                            [review.login, review.description, review.isBaiting, review.roadQuality,review.fishingTime, review.raiting, review.latitude, review.longitude])
+    })
+    .then((result) => {
+        const reviewId = result.rows[0].id
+        console.log(reviewId)
+       
+        review.facts.forEach(fact => {
+            fact.unshift(reviewId)
+        })
+       return  client.query(format('INSERT INTO facts (review, method, bait, fish) VALUES %L returning review', review.facts))
+    })
+    .then((result) => {
+        const reviewId = result.rows[0].review
+        if (req.files.length !== 0) {
+            const values = []
+            req.files.forEach(file => {
+                values.push([reviewId, `uploads/reviews/${review.login}/`+ file.filename])
+            })
+
+            return client.query(format('INSERT INTO reviewphotos (review, link) VALUES %L', values))
+            console.log(values)
+        }
+    })
+    .then((result) => {
+        return client.query('commit')
+    })
+    .then((result) => {
+        console.log('Транзакция вроде сработала')
+        result = { status: 'ok' }
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log('Ошибка в транзакции, откат: ', err)
+        return client.query('ROLLBACK')
+    })
+    .catch((err) => {
+        console.log('Ошибка во время отката: ', err)
+    })
 }
+
 
 function transformFacts(facts) {
     let transformedFacts = []
 
     facts.forEach(fact => {
-        let fishes = []
         fact.fishes.forEach(fish => {
-            fishes.push(fish.id)
+            // transformedFacts.push({method: fact.method.id, bait: fact.bait.id, fish: fish.id})
+            transformedFacts.push([fact.method.id, fact.bait.id, fish.id])
         })
-
-        console.log(fishes)
-        transformedFacts.push({method: fact.method.id, bait: fact.bait.id, fishes: fishes})
     })
 
     return transformedFacts
